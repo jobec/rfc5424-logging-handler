da13264aba28f98aa436151af524d066df3591d2
Joris Beckers <joris.beckers@gmail.com>
2017-01-18 22:16:42 +0100

    Included python 2.7 compatibility

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
