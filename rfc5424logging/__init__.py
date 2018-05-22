6a2bba42de2a28d82bcff9d9904b330eb0b3ae77
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-05-22 15:12:24 -0500

    loading the new class to __all__

--- rfc5424logging/__init__.py
+++ rfc5424logging/__init__.py
@@ -1,6 +1,14 @@
-from .handler import Rfc5424SysLogHandler, NILVALUE
+from .handler import Rfc5424SysLogHandler, TlsRfc5424SysLogHandler, NILVALUE
 from .adapter import Rfc5424SysLogAdapter, EMERGENCY, ALERT, NOTICE
 
 __version__ = "1.1.2"
 
-__all__ = ['Rfc5424SysLogHandler', 'Rfc5424SysLogAdapter', 'EMERGENCY', 'ALERT', 'NOTICE', 'NILVALUE']
+__all__ = [
+    'Rfc5424SysLogHandler',
+    'Rfc5424SysLogAdapter',
+    'TlsRfc5424SysLogHandler',
+    'EMERGENCY', 
+    'ALERT',
+    'NOTICE',
+    'NILVALUE'
+]
